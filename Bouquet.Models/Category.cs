0f9d2bcf710a56d84deee3512de50cb04c44f862
Alex Master <mastrukov@yahoo.ca>
2020-03-15 16:57:49 -0400

    added Repository

---
+++ Bouquet.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bouquet.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+
+    }
+}
