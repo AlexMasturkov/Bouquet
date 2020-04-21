7334758ba25481278a93b55e54af0b539439b076
Alex Master <mastrukov@yahoo.ca>
2020-04-21 13:00:59 -0400

    Added component to display User Name

---
+++ Bouquet/Areas/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Bouquet.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Bouquet.Areas.ViewComponents
+{
+    public class UserNameViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserNameViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userFromDb = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claims.Value);
+
+            return await Task.Run(() => View(userFromDb));
+        }
+    }
+}
